537485e70dd008283df56c1d06fc7ba4c30bce4e
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2024-11-30 08:26:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002490259645')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+Dgs6AP3nKJE2NjBl') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-IS_VERIFY = is_enabled('IS_VERIFY', False)
+IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/")
