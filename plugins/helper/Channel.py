d63cf95bf57f3c4a1a523e85a3d4b7bca007e138
owajbhai <owajshaikhrcm@gmail.com>
2025-04-17 14:16:42 +0530

    Update Channel.py

--- plugins/helper/Channel.py
+++ plugins/helper/Channel.py
@@ -9,7 +9,7 @@ CHANNEL_ID = -1002481796569
 async def add_button(client, message):
     if message.chat.id == CHANNEL_ID:
         button = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔰𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽🔰", url="https://t.me/Movie_Stores_hub")]]
+            [[InlineKeyboardButton("🔰𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽🔰", url="https://t.me/+YNF4Jt81AuI5ZjBl")]]
         )
         
         try:
