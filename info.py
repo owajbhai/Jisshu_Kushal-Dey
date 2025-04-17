cd99c412c2c3980180c4dbc349ed1dce1e1bc3a6
owajbhai <owajshaikhrcm@gmail.com>
2025-04-17 08:08:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5651012552').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/botmaster55") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/Kushal_dey") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002556269614'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+YNF4Jt81AuI5ZjBl')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002503269033').split()]
